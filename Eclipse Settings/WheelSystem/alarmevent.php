aac67762bdf5dcb1b380cbadf945d46acf27e803
Bhekisizwe <tshomie2020@yahoo.com>
2019-01-13 15:50:29 +0200

    Fault Report Server API
    Created and Tested Server API for Fault Report generation

--- Eclipse Settings/WheelSystem/alarmevent.php
+++ Eclipse Settings/WheelSystem/alarmevent.php
@@ -73,4 +73,65 @@ use UserClasses\BusinessLayer\ManageSession;
         ->withBody($body); 
     });
     
+    //Generate Coach Stoppage Statistics Report
+    $app->get('/faultreport/{daterange}', function (Request $request, Response $response, array $args) {
+        //Create Objects
+        $alarm=new AlarmEventLogger();
+        $alarmBO=new WheelMeasurementsComparisonBO();
+        $userrole=new UserRole();
+        $userroleBO=new UserRoleBO();
+        $searchdatesstr=$args["daterange"];
+        $date_arr=explode("_",$searchdatesstr);
+        $arr=$alarmBO->getArray();
+        $manageSession=new ManageSession();
+        if(isset($_SESSION["lastActive"])) $manageSession->determineSessionValidity(time());
+        if(isset($_SESSION["staffNumber"])){
+            $userrole_arr["userRole2DArray"][0]["roleID"]=$_SESSION["roleID"];
+            $userroleBO->set($userrole_arr);
+            $accessRight="R";
+            $activityName="Wheel Measurements Management";
+            if($userrole->checkUserAuthorization($userroleBO, $accessRight, $activityName)){
+                $arr=array();
+                $alarmBO->setStaffNumber($_SESSION["staffNumber"]);
+                $alarmBO->setAlarmSearchStartDate($date_arr[0]);
+                $alarmBO->setAlarmSearchEndDate($date_arr[1]);
+                $alarm->generateFaultReport($alarmBO);
+                $alarmBO->setTransactionStatus(true);   
+                $arr=$alarmBO->getArray();
+            }
+            else{
+                $arr=array();
+                $arr_err=array();
+                $arr_err["errorCode"]="0x18";
+                $arr_err["errorDescription"]="You have no access rights to carry out the action you attempted. Please contact the administrator to resolve this.";
+                $arr_error["errorAssocArray"]=$arr_err;
+                $alarmBO->set($arr_error);
+                $arr=$alarmBO->getArray();
+            }
+            $_SESSION["lastActive"]=time();
+        }
+        else{
+            $alarmBO->setTransactionStatus(false);
+            $arr=array();
+            //write Error Code and Description
+            $arr_err=array();
+            $arr_err["errorCode"]="0x19";
+            $arr_err["errorDescription"]="Session has expired";
+            $arr_error["errorAssocArray"]=$arr_err;
+            $alarmBO->set($arr_error);
+            $arr=$alarmBO->getArray();
+        }
+        $arr_json=json_encode($arr);
+        //destroy objects
+        unset($manageSession);
+        unset($alarm);
+        unset($alarmBO);
+        unset($userrole);
+        unset($userroleBO);
+        $body=$response->getBody();
+        $body->write($arr_json);
+        return $response->withHeader("Content-Type", "application/json;charset=UTF-8")
+        ->withBody($body);
+    });
+    
 ?>
--- Eclipse Settings/WheelSystem/vendor/UserClasses/BusinessLayer/src/AlarmEventLogger.php
+++ Eclipse Settings/WheelSystem/vendor/UserClasses/BusinessLayer/src/AlarmEventLogger.php
@@ -5,24 +5,33 @@ namespace UserClasses\BusinessLayer;
 use UserClasses\BusinessObjects\ActivityLogBO;
 use UserClasses\DataLayer\AlarmEventLoggerDL;
 use UserClasses\BusinessObjects\WheelMeasurementsComparisonBO;
+use UserClasses\BusinessObjects\UserAccountBO;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 /**
  *
  * @author Bheki Mthethwa
  *        
  */
-class AlarmEventLogger
+class AlarmEventLogger extends FileHandler
 {
     private $activityLog;
+    private static $dirPath=__DIR__."\\..\\..\\..\\..\\FaultReport";
     private $activityBO;
     private $alarmEventDL;
     private $err;
+    private $sender;
+    private $accountBO;
 
     /**
      */
     public function __construct()
     {
+        Parent::__construct(self::$dirPath, "");
         $this->err=new ErrorLog();
+        $this->accountBO=new UserAccountBO();
+        $this->sender=new Email();
         $this->activityLog=new ActivityLog();
         $this->activityBO=new ActivityLogBO();
         $this->alarmEventDL=new AlarmEventLoggerDL();
@@ -33,9 +42,11 @@ class AlarmEventLogger
     function __destruct()
     {
         $this->err=null;
+        $this->accountBO=null;
+        $this->sender=null;
         $this->activityBO=null;
         $this->activityLog=null;
-        $this->alarmEventDL=null;
+        $this->alarmEventDL=null;        
     }
     
     public function searchAlarmEvents(WheelMeasurementsComparisonBO $data):array {
@@ -50,7 +61,118 @@ class AlarmEventLogger
             }
             return $arr_result;
         }
+        else{
+            $arr=array();
+            return $arr;
+        }
+    }
+    
+    public function generateFaultReport(WheelMeasurementsComparisonBO $data) {
+        if(isset($data)){  
+            $arr_search_keys=array("Flange Height",
+                "Flange Width",
+                "Toe Creep",
+                "Hollowing",
+                "Gibson Ring",
+                "Cut Tyre Depth",
+                "Cut Tyre Width",
+                "Cut Tyre Distance From Flange",
+                "Wheel Skid",
+                "Spread Rim"
+            );
+            $arr_faults=array();    //this will store the fault count in the required order
+            $arr_column_names=array(); //Column Names of Excel Spreadsheet Report
+            foreach($arr_search_keys as $value){
+                $arr_column_names[]=$value." Stops";
+            }
+            $arr_faults[0]=$arr_column_names;
+            $arr_faults[0][]="Report Start Date";
+            $arr_faults[0][]="Report End Date";
+            $arr=$this->searchAlarmEvents($data);
+            //we need to search for all 10 possible stoppage causes and count them
+            $total_failures=0;
+            for($i=0;$i<count($arr_search_keys);$i++){
+                $counter=0; //reset counter that counts number of failure causes.
+                for($rows=0;$rows<count($arr);$rows++){
+                    //go through each row and search the alarmCause column for the cause of failure
+                    $pattern="/(.)*".$arr_search_keys[$i]."(.)*/";
+                    if($arr[$rows]["predictedWheelFailureDate"]==$arr[$rows]["referenceDate"]){
+                        if(preg_match($pattern, $arr[$rows]["alarmCause"])===1){
+                            $counter+=1;    //then we know the cause of stoppage has been found
+                        }
+                    }                    
+                }
+                $arr_faults[1][$i]=$counter;
+                $total_failures+=$counter;
+            }
+            $arr_faults[1][]=$data->getAlarmSearchStartDate();
+            $arr_faults[1][]=$data->getAlarmSearchEndDate();
+            $this->writeReportToMSExcel($arr_faults, $data->getStaffNumber(),$total_failures);
+        }
         else return NULL;
+    }
+    
+    private function writeReportToMSExcel(array $reportData,string $staffNumber,int $total_failures) {
+        if(count($reportData)>0 && $staffNumber!=""){
+            try {
+                //check if Directory Exists first
+                $filename="report_".$staffNumber.".xlsx";
+                if($this->checkDirectoryExists()){
+                    //it does. then check if File exists
+                    if($this->checkFileExists($filename)){
+                        //if it does, delete it
+                        $this->deleteFile($filename);
+                    }
+                    //write data to MSExcel 2007 and Later
+                    $this->writeToExcel($reportData,$filename,$total_failures);
+                    $this->accountBO->setStaffNumber($staffNumber);
+                    $userAccount=$this->getEmailRecepient($this->accountBO);                    
+                    $email_message=$this->generateEmailMessage($userAccount, $filename);
+                    $this->sender->sendEmail($email_message);                    
+                }
+                else{
+                    //it does not. Create It!
+                    if($this->createDirectory()){
+                        //write data to MSExcel 2007 and Later
+                        $this->writeToExcel($reportData,$filename,$total_failures);
+                        $this->accountBO->setStaffNumber($staffNumber);
+                        $userAccount=$this->getEmailRecepient($this->accountBO);
+                        $email_message=$this->generateEmailMessage($userAccount, $filename);
+                        $this->sender->sendEmail($email_message);                    
+                    }
+                }
+            } catch (\Exception $e) {
+                $class_name="AlarmEventLogger";
+                $method_name="writeReportToMSExcel";
+                $this->err->logErrors($e,null,$class_name, $method_name);
+            }
+        }
+    }
+    
+    private function writeToExcel(array $reportData,string $filename,int $total_failures){
+        $workBook=new Spreadsheet();
+        $alphabet=range("A", "Z");      //create letters of the Alphabet.
+        $sheet=$workBook->getActiveSheet();
+        for($row=0;$row<count($reportData);$row++){
+            //$row is the row position of the report
+            for($column=0;$column<count($reportData[$row]);$column++){
+                //$column is the column position of the report
+                if($total_failures>0 && $column<(count($reportData[$row])-2) && $row>0){
+                    $perc=round(($reportData[$row][$column]/$total_failures)*100,2);
+                    $cellData=$reportData[$row][$column]." (".$perc."%)";
+                }
+                else $cellData=$reportData[$row][$column];
+                
+                $sheet->setCellValue($alphabet[$column]."".($row+1), $cellData);
+            }
+        }
+        $writer=new Xlsx($workBook);
+        $fullFilePath=self::$dirPath."\\".$filename;
+        $writer->save($fullFilePath);
+        //destroy objects
+        unset($writer);
+        unset($sheet);
+        unset($workBook);
     }
     
     private function checkAlarmEventExists(WheelMeasurementsComparisonBO $data):bool {
@@ -123,6 +245,31 @@ class AlarmEventLogger
             return $status_message;
         }
         else return false;
+    }
+    
+    private function getEmailRecepient(UserAccountBO $data):array {
+        if(isset($data)){
+            $userAccounts=new UserAccounts();
+            //extract email address using staffNumber of user.
+            $arr_result=$userAccounts->listUserAccount($data);
+            unset($userAccounts);
+            return $arr_result;
+        }
+        else return NULL;
+    }
+    
+    private function generateEmailMessage(array $arr,string $filename):array {
+        //generate email array
+        $arr_email=array();
+        $arr_email["to"]=$arr["emailAddress"];
+        $arr_email["subject"]="Fault Statistics Report";
+        $body="Dear Sir/Madam<p>";
+        $body.="Please find attached the requested Fault distribution statistics report. <p>";
+        $body.=" By Order (Gqunsu Engineering Pty Ltd)";
+        $arr_email["body"]=$body;
+        $arr_email["file"]=self::$dirPath."\\".$filename;
+        unset($data);
+        return $arr_email;
     }
 }
 
